942fba3136ca0f7feec6663a20652ee77872440b
Matt Wilhelm <foulerfire@gmail.com>
2026-02-01 14:22:35 -0500

    Santized input on client and server side, included erros for server side

--- Lab3/index.php
+++ Lab3/index.php
@@ -1,29 +1,29 @@
-<?php   ?>
+<?php //start file for lab3   ?>
 
-//start file for lab3
 <main>
-    <h2> Hello this is a test </h2>
+    <h2> Bake It Till You Make It - Contact Form     </h2>
     
     <form action = "processForm.php" method="post">
     <fieldset>
-        <!--- client side validation with html -->
+        <!-- client input with client side validation with html -->
         <legend> List test </legend>
-        <label for="first_name">First name</label>
-        <input type="text" id="first_name" name="first_name">
-        <label for="last_name">Last name</label>
-        <input type="text" id="last_name" name="last_name">
-        <label for="email">Email</label>
-        <input type="email" id="email" name="email" >
-
+        <label for="first_name">First name</label> 
+        <input type="text" id="first_name" name="first_name" required>
+        <label for="last_name">Last name</label> 
+        <input type="text" id="last_name" name="last_name" required>
+        
+        <br> <label for="email">Email</label> </br>
+        <input type="email" id="email" name="email" required>
+        <!-- message input box, not a required to be filled out -->
         <p>
             <label for="message">Message</label><br>
             <textarea id="message" name="message" rows="4"
-            placeholder="Allergies, delivery instructions, custom messages..."></textarea>
+            placeholder="Please type your message here" required></textarea>
         </p>
         </fieldset>
-
+        <!--- button to send message -->
         <p>
-        <button type="submit">Place Order</button>
+        <button type="submit">Send Message</button>
         </p>
 </form>
 </main>
--- Lab3/processForm.php
+++ Lab3/processForm.php
@@ -1,13 +1,53 @@
-<? php
-
-
+<?php
 //grab the data, clean and store in a variable
 $firstName = filter_input(INPUT_POST, 'first_name', FILTER_SANITIZE_SPECIAL_CHARS);
 $lastName  = filter_input(INPUT_POST, 'last_name', FILTER_SANITIZE_SPECIAL_CHARS);
-$address = filter_input(INPUT_POST, 'address', FILTER_SANITIZE_SPECIAL_CHARS);
 $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_SPECIAL_CHARS);
+
+//validate serverside
+$errors = [];
+
+//require text fields
+//first name validation
+if ($firstName === null || $firstName === ''){
+    $errors[] = "First name is required";
+}
+//last name validation
+if ($lastName === null || $lastName === ''){
+    $errors[] = "Last name is required";
+}
+//email validation
+if ($email=== null || $email === ''){
+    $errors[] = "email is required";
+} elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $errors[] = "You must enter a valid email";
+}
+// message validation
+if ($message === null || $message === ''){
+    $errors[] = "Message is required";
+}
+//loop throuhg error messages
+if (!empty($errors)) {
+    echo "<ul>";
+    foreach ($errors as $error) {
+        echo "<li>$error</li>";
+    }
+    echo "</ul>";
+    exit;
+}
+?>
+
+<main>
+<!-- validation confirmation message with user enter information -->
+<?php 
+    echo "<h2> Thank you for contacting us " . $firstName . " ". $lastName. "</h2>"; 
+    echo "<h2>The message you sent was</h2>";
+    echo "<p>" . ($message) . "</p>";
+?>
+
+</main>
 
 
 
 
->
