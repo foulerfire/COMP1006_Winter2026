d6836430a0b8513ef2a5f90e8872a38c8c19ba0f
Matt Wilhelm <foulerfire@gmail.com>
2026-02-01 13:21:52 -0500

    Added starting form fields and started code for server side

--- Lab3/index.php
+++ Lab3/index.php
@@ -1,5 +1,30 @@
-<?php
+<?php   ?>
+
 //start file for lab3
+<main>
+    <h2> Hello this is a test </h2>
+    
+    <form action = "processForm.php" method="post">
+    <fieldset>
+        <!--- client side validation with html -->
+        <legend> List test </legend>
+        <label for="first_name">First name</label>
+        <input type="text" id="first_name" name="first_name">
+        <label for="last_name">Last name</label>
+        <input type="text" id="last_name" name="last_name">
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" >
 
+        <p>
+            <label for="message">Message</label><br>
+            <textarea id="message" name="message" rows="4"
+            placeholder="Allergies, delivery instructions, custom messages..."></textarea>
+        </p>
+        </fieldset>
 
->
+        <p>
+        <button type="submit">Place Order</button>
+        </p>
+</form>
+</main>
+
---
+++ Lab3/processForm.php
@@ -0,0 +1,13 @@
+<? php
+
+
+//grab the data, clean and store in a variable
+$firstName = filter_input(INPUT_POST, 'first_name', FILTER_SANITIZE_SPECIAL_CHARS);
+$lastName  = filter_input(INPUT_POST, 'last_name', FILTER_SANITIZE_SPECIAL_CHARS);
+$address = filter_input(INPUT_POST, 'address', FILTER_SANITIZE_SPECIAL_CHARS);
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+
+
+
+
+>
