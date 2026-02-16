1d7aad40e05e8c3e756791f2c1435ece37c88145
Matt Wilhelm <foulerfire@gmail.com>
2026-02-16 12:32:25 -0500

    Created starting assets for project including index and header

---
+++ assignments/CourseProject/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    require "includes/header.php";
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esports Team</title>
+</head>
+
+<body>
+    <main class="container mt-4">
+        <h1>Please</h1>
+
+        <form action="process.php" method="post" class="mt-3">
+            <label class="form-label" for="first_name">First Name</label>
+            <input class="form-control" type="text" id="first_name" name="first_name">
+
+            <label class="form-label mt-3" for="last_name">Last Name</label>
+            <input class="form-control" type="text" id="last_name" name="last_name">
+
+            <label class="form-label mt-3" for="last_name">Position</label>
+            <input class="form-control" type="text" id="position" name="position">
+
+
+            <label class="form-label mt-3" for="email">Email Address</label>
+            <input class="form-control" type="email" id="email" name="email">
+
+            <label for="phone" class="form-label">Phone number</label>
+            <input type="tel" id="phone" name="phone" placeholder="555-123-4567" class="form-control">
+
+            <button class="btn btn-primary mt-4" type="submit">Subscribe</button>
+        </form>
+
+        <p class="mt-4">
+            <a href="subscribers.php">View Subscribers</a>
+        </p>
+    </main>
+</body>
+
+</html>
