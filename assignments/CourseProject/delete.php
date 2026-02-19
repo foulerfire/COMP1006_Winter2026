ebedf32201bf033227d3c637821d3d969afe4615
Matt Wilhelm <foulerfire@gmail.com>
2026-02-19 15:30:13 -0500

    Created delete code following code from lesson 6 part 4

---
+++ assignments/CourseProject/delete.php
@@ -0,0 +1,24 @@
+<?php
+// deletes a player from the database using the player id
+
+// database connection
+require "includes/connect.php";
+
+// make sure an id was passed in the url
+$playerId = $_GET['id'];
+
+// sql query to delete player
+$sql = "DELETE FROM players WHERE player_id = :player_id";
+
+// prepare sql statement
+$stmt = $db->prepare($sql);
+
+// bind player id to query
+$stmt->bindParam(':player_id', $playerId);
+
+// execute delete
+$stmt->execute();
+
+// redirect back to players list
+header("Location: players.php");
+exit;
