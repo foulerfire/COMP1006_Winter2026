ca258d20c2505d1fd91d54439fc7c355bb9d867d
Matt Wilhelm <foulerfire@gmail.com>
2026-03-22 16:24:48 -0400

    Created upload form page and uploads folder and added file upload form

---
+++ assignments/labs/Lab5/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Upload Image</title>
+</head>
+<body>
+
+<h2>Upload Profile Picture</h2>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="profileImage">
+    <button type="submit">Upload</button>
+</form>
+
+</body>
+</html>
