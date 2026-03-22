136d3571567a7c56e801cee8f607915073ff1ea7
Matt Wilhelm <foulerfire@gmail.com>
2026-03-22 16:54:38 -0400

    Implemented file upload logic

--- assignments/labs/Lab5/upload.php
+++ assignments/labs/Lab5/upload.php
@@ -6,3 +6,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     echo "<p>Form submitted successfully</p>";
 }
+
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $file = $_FILES['profileImage'];
+
+    $fileName = $file['name'];
+    $tempName = $file['tmp_name'];
+
+    $uploadPath = "uploads/" . $fileName;
+
+    if (move_uploaded_file($tempName, $uploadPath)) {
+        echo "<p>Upload successful!</p>";
+    } else {
+        echo "<p>Upload failed.</p>";
+    }
+}
