4aea4a547fec790fe9acb6bc0ce21cc6b671f656
Matt Wilhelm <foulerfire@gmail.com>
2026-03-22 21:02:09 -0400

    cleaned up code and added comments

--- assignments/labs/Lab5/upload.php
+++ assignments/labs/Lab5/upload.php
@@ -1,25 +1,26 @@
 <?php
 
+// check if form was submitted
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
+    // get uploaded file info
     $file = $_FILES['profileImage'];
 
-    echo "<p>Form submitted successfully</p>";
-}
-
-<?php
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-    $file = $_FILES['profileImage'];
-
+    // store file name and temp location
     $fileName = $file['name'];
     $tempName = $file['tmp_name'];
 
+    // set upload path
     $uploadPath = "uploads/" . $fileName;
 
+    // move file to uploads folder
     if (move_uploaded_file($tempName, $uploadPath)) {
+
         echo "<p>Upload successful!</p>";
+
+        // display uploaded image
+        echo "<img src='$uploadPath' width='200'>";
+
     } else {
         echo "<p>Upload failed.</p>";
     }
